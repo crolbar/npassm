7cb3524e22154e3827acb134a42d4e9943e9ae57
crolbar <crolbar@crolbar.com>
2024-08-19 08:08:41 +0300

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,27 +1,59 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-          buildInputs = with pkgs; [
-            pkg-config
-            gnumake
-            cmake
-            gcc
-            openssl
-            ncurses
-          ];
+  outputs = {nixpkgs, ...}: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+    eachSystem = nixpkgs.lib.genAttrs systems;
+
+    pkgsFor = eachSystem (
+      system:
+        import nixpkgs {inherit system;}
+    );
+
+    mkBulidInputs = pkgs:
+      with pkgs; [
+        clang-tools
+        pkg-config
+        gnumake
+        cmake
+        gcc
+        openssl
+        ncurses
+      ];
+  in {
+    devShells = eachSystem (
+      system: let
+        pkgs = pkgsFor.${system};
+        nativeBuildInputs = mkBulidInputs pkgs;
       in
-      {
-        devShells.default = pkgs.mkShell { inherit buildInputs; };
-        defaultPackage = pkgs.callPackage ./default.nix {};
+        with pkgs; {
+          default = mkShell {
+            inherit nativeBuildInputs;
+          };
+        }
+    );
+
+    packages = eachSystem (
+      system: let
+        pkgs = pkgsFor.${system};
+        nativeBuildInputs = mkBulidInputs pkgs;
+
+        fs = nixpkgs.lib.fileset;
+
+        npassm = pkgs.stdenv.mkDerivation {
+          pname = "npassm";
+          version = "0.1";
+          src = fs.toSource {
+            root = ./.;
+            fileset = fs.difference ./. (fs.maybeMissing ./build);
+          };
+
+          inherit nativeBuildInputs;
+        };
+      in {
+        inherit npassm;
+        default = npassm;
       }
     );
+  };
 }
